4ad9dc90f2eb9c5cc11babad7f07c81943d31ed2
adri5104 <92983875+adri5104@users.noreply.github.com>
2022-04-22 14:32:21 +0200

    Pantallita metida

--- include/CTPro.h
+++ include/CTPro.h
@@ -35,4 +35,10 @@
     const uint8_t TOF_SENSOR_LEFT = 0x31;
     const uint8_t TOF_SENSOR_RIGHT = 0x32;
 
+
+    #define SCREEN_WIDTH 128 // OLED display width, in pixels
+    #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+    #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+    #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 #include "CTPro.h"
 #include "Motor.h"
 #include "TofSensors.h"
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 Motor motorL = Motor(DIR_LA, DIR_LB, PWM_L);
@@ -9,6 +15,19 @@ Motor motorR = Motor(DIR_RA, DIR_RB, PWM_R);
 TofSensors mySensors(XSH_1, XSH_2, XSH_3);
 
 void setup(){
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); 
+  }
+  display.display();
+  delay(2000); 
+
+  // Clear the buffer
+  display.clearDisplay();
+  display.display();
+  delay(2000); 
+  display.clearDisplay();
   mySensors.init();
   
   mySensors.setID(
